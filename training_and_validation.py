c5e08a86d626cd9bc974377bae27bf85de3be713
Ivan D'Onofrio <d.onofrio.ivan@gmail.com>
2021-02-09 09:58:57 +0100

    Bugfixes

--- training_and_validation.py
+++ training_and_validation.py
@@ -25,7 +25,7 @@ def train(model, optimizer, X, A, norm, weights, n):
     return loss.item()
 
 
-def train_anomaly_detection(model, optimizer, X, A, n):
+def train_anomaly_detection(model, optimizer, X, A, weights, norm, n):
 
     # Set the VGAE to be trained
     model.train()
@@ -37,7 +37,7 @@ def train_anomaly_detection(model, optimizer, X, A, n):
     A_rec, X_rec, mu, log_var = model(X)
 
     # MSE + KLD loss
-    loss, reconstruction_errors = loss_function_naive_anomaly_detection(X_rec, A_rec, X, A, .4, n, mu, log_var)
+    loss, reconstruction_errors = loss_function_naive_anomaly_detection(X_rec, A_rec, X, A.to_dense(), .5, num_nodes, mu, log_var)
     local_reconstruction_errors = reconstruction_errors.cpu().numpy()
 
     # Optimization step
